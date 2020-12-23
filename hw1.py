7b826b0f45448df0e112552e5c87930d4ddb0a73
S79-coder <76443603+S79-coder@users.noreply.github.com>
2020-12-24 02:16:35 +0300

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,32 @@
+a=96
+b=24.12
+c="Selçuk"
+d=[5,10,15]
+e=True
+
+print(a,b,c,d,e)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print(f'Hello {c}')
+
+
+f=input("Please enter a value: ")
+g=input("Please enter a value: ")
+h=input("Please enter a value: ")
+ı=input("Please enter a value: ")
+j=input("Please enter a value: ")
+
+print(f,g,h,ı,j)
+
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(ı))
+print(type(j))
+
+print(f'Hello {h}')
---
+++ hw1shell.py
@@ -0,0 +1,24 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: C:\Users\Selçuk\Desktop\hw1.py ===================
+96 24.12 Selçuk [5, 10, 15] True
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'list'>
+<class 'bool'>
+Hello Selçuk
+Please enter a value: 96
+Please enter a value: 24.12
+Please enter a value: Selçuk
+Please enter a value: [5,10,15]
+Please enter a value: True
+96 24.12 Selçuk [5,10,15] True
+<class 'str'>
+<class 'str'>
+<class 'str'>
+<class 'str'>
+<class 'str'>
+Hello Selçuk
+>>> 
